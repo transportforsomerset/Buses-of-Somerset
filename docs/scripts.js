36283614ca33f7751451fb07380f2b242585ff2a
UKChris-osm <59393694+UKChris-osm@users.noreply.github.com>
2023-03-04 20:50:44 +0000

    Update scripts.js

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,1 +1,5 @@
+var map = L.map('map').setView([51.02, -3.1], 13);
 
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
