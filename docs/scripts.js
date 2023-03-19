06e11b740c6b80d7c7497af2d35c9729badb872e
UKChris-osm <59393694+UKChris-osm@users.noreply.github.com>
2023-03-19 23:00:41 +0000

    Update scripts.js

--- docs/scripts.js
+++ docs/scripts.js
@@ -13,10 +13,10 @@ function whatamiwatching() {
   else
     localStorage.setItem("watchRoute", watchRoute);
   
-  const whatisbeingwatched = document.getElementById("whatisbeingwatched");
-  
   const para = document.createElement("p");
   const paratext1 = document.createTextNode("Currently watching route: " + watchRoute);
-  para.replaceChild(paratext1);
-  whatisbeingwatched.appendChild(para);
+  para.appendChild(paratext1);
+  
+  const whatisbeingwatched = document.getElementById("whatisbeingwatched");
+  whatisbeingwatched.replaceChild(para);
 }
